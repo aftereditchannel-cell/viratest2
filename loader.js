708571c6971c9c3e2cd6c058576fbd5074ce1a9f
aftereditchannel-cell <afteredit.channel@gmail.com>
2026-02-17 00:19:50 +0330

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,7 +1,24 @@
 window.addEventListener("load", ()=>{
+
     const loader = document.getElementById("loader");
+
     setTimeout(()=>{
         loader.classList.add("loader-hide");
-        setTimeout(()=>{loader.remove();},900);
+
+        setTimeout(()=>{
+
+            const session = localStorage.getItem("vira_session");
+
+            if(session){
+                // قبلاً وارد شده
+                window.location.href = "home.html";
+            }else{
+                // اولین ورود
+                window.location.href = "auth.html";
+            }
+
+        },900);
+
     },1800);
+
 });
