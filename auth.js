3a6cdd4138da37bf9ebcb77d3184e2348a4b49b0
aftereditchannel-cell <afteredit.channel@gmail.com>
2026-02-17 02:17:36 +0330

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,30 +1,11 @@
 // ورود مهمان
 document.getElementById("guestLogin").onclick = () => {
-
     const user = {
         type: "guest",
         name: "کاربر مهمان",
         id: "guest_" + Date.now(),
         login: new Date().toISOString()
     };
-
     localStorage.setItem("vira_session", JSON.stringify(user));
-
-    window.location.href = "home.html";
-};
-
-
-// ثبت نام با گوگل (شبیه سازی)
-document.getElementById("googleLogin").onclick = () => {
-
-    const user = {
-        type: "google",
-        name: "کاربر گوگل",
-        id: "google_" + Date.now(),
-        login: new Date().toISOString()
-    };
-
-    localStorage.setItem("vira_session", JSON.stringify(user));
-
     window.location.href = "home.html";
 };
