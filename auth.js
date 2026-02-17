4f933ec207e7b76d816c501e793e75d3916bd32f
aftereditchannel-cell <afteredit.channel@gmail.com>
2026-02-17 03:30:10 +0330

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,24 +1,29 @@
 // ورود مهمان
 document.getElementById("guestLogin").onclick = () => {
+
     const user = {
         type: "guest",
         name: "کاربر مهمان",
         id: "guest_" + Date.now(),
         login: new Date().toISOString()
     };
+
     localStorage.setItem("vira_session", JSON.stringify(user));
+
     window.location.href = "home.html";
 };
 
-// پردازش گوگل: اگر URL حاوی user باشد
-const params = new URLSearchParams(window.location.search);
-if(params.has('user')){
-    const user = JSON.parse(decodeURIComponent(params.get('user')));
-    localStorage.setItem("vira_session", JSON.stringify(user));
-    window.history.replaceState({}, document.title, "home.html"); // پاک کردن query از URL
-}
+// ثبت نام با گوگل (شبیه سازی)
+document.getElementById("googleLogin").onclick = () => {
 
-// لینک امن گوگل برای موبایل و دسکتاپ
-document.getElementById("googleLogin").addEventListener("click", () => {
-    window.location.assign("https://viratest2.onrender.com/auth/google");
-});
+    const user = {
+        type: "google",
+        name: "کاربر گوگل",
+        id: "google_" + Date.now(),
+        login: new Date().toISOString()
+    };
+
+    localStorage.setItem("vira_session", JSON.stringify(user));
+
+    window.location.href = "home.html";
+};
